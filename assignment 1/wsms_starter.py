f023a1ca528099fac896cc927a251277aa5e73f5
Jeff Whitworth <jwhitwo@gmail.com>
2021-02-04 15:18:21 -0500

    Create wsms_starter.py
    Added starter file

---
+++ assignment 1/wsms_starter.py
@@ -0,0 +1,37 @@
+# CSC 339-02 Spring 2021
+# Program 1
+# Name: 
+
+def load_stock(filename):
+    # Reads filename and adds contents to inventory
+    pass
+
+def check_stock(item):
+    # Returns the in stock quantity of an item
+    pass
+    
+def add_stock(item,qty):
+    # Increments the stock of the item by qty
+    pass
+    
+def remove_stock(item,qty):
+    # Decrements the stock of the item by qty
+    pass
+    
+def pull_order(filename):
+    # Reserves the order specified by filename
+    # outputs the ordered bin pull list
+    pass
+
+def fill_order(filename):
+    # Deletes the specified pull list from the system
+    pass
+    
+def restock_order(filename):
+    # Reads the given pull list to reverse the order
+    # Deletes the pull list from the system
+    pass
+    
+def print_stock():
+    # Prints the stock of each bin
+    pass
