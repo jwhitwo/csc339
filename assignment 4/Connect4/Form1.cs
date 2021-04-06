e7b409222aeaeba1e43ff4cc7165ac5725c6922d
Jeff Whitworth <jwhitwo@gmail.com>
2021-04-05 21:55:17 -0400

    Added assignment 4 starter code.

---
+++ assignment 4/Connect4/Form1.cs
@@ -0,0 +1,29 @@
+﻿// Name: (Your Name)
+// CSC339 - Spring 2021
+// Assignment 4
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
